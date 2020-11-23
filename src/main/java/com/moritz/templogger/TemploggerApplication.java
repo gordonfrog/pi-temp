55c88cd491d17bdba36ef527f8622e663894fe39
gordonfrog <gordonfrog@gordonfrogs-MacBook-Pro.local>
2020-11-23 14:22:32 -0700

    testing on pi

--- src/main/java/com/moritz/templogger/TemploggerApplication.java
+++ src/main/java/com/moritz/templogger/TemploggerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
+@EnableScheduling
 public class TemploggerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/moritz/templogger/resource/TemperatureResource.java
+++ src/main/java/com/moritz/templogger/resource/TemperatureResource.java
@@ -1,5 +1,7 @@
 package com.moritz.templogger.resource;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -7,6 +9,7 @@ import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.moritz.templogger.db.TemperatureEntity;
 import com.moritz.templogger.db.TemperatureRepository;
+import com.pi4j.component.temperature.TemperatureSensor;
+import com.pi4j.io.w1.W1Master;
+import com.pi4j.temperature.TemperatureScale;
 
 @RestController
 public class TemperatureResource {
@@ -26,6 +32,21 @@ public class TemperatureResource {
     	List<TemperatureEntity> result = StreamSupport.stream(temperatureRepository.findAll().spliterator(), false)
     			    .collect(Collectors.toList());
         return result;
+    }
+    
+    @Scheduled(initialDelay = 500, fixedRate = 10000) //poll pi every 10sec to display the graph
+    private void runPiTempCheck() {
+    	W1Master w1Master = new W1Master();
+    	for (TemperatureSensor device : w1Master.getDevices(TemperatureSensor.class)) {
+            Double d = device.getTemperature(TemperatureScale.FARENHEIT);
+
+            TemperatureEntity temperatureEntity = new TemperatureEntity();
+            temperatureEntity.setTemperature(d);
+            temperatureEntity.setTime(Timestamp.valueOf(LocalDateTime.now()));
+
+            temperatureRepository.save(temperatureEntity);
+            System.out.println("Logged temp: "+temperatureEntity);
+        }
     }
 
 //    @PostMapping("/measurement")
